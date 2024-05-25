7fa052526fff03b0217788b81247236646456179
kshitij jha <84563720+trivialguy@users.noreply.github.com>
2024-05-25 22:33:53 +0530

    Update AssetMarket.sol

--- contracts/AssetMarket.sol
+++ contracts/AssetMarket.sol
@@ -6,20 +6,6 @@ import "./Asset.sol";
 contract Market {
 
     address[] private _assets;
-
-    function createcontent(string memory _name, string memory _symbol) public returns (address) {
-        Asset asset = new Asset(_name, _symbol);
-        _assets.push(address(asset));
-        return address(asset);
-    }
-
-    function mintasset(address assetAddress, uint amount) public {
-        Asset asset = Asset(assetAddress);
-        asset.mint(msg.sender, amount);
-        asset.approve(address(this), amount);
-    }
-
-
     enum Listingstatus {
         Active,
         Sold,
@@ -29,34 +15,51 @@ contract Market {
     struct Listing {
         Listingstatus status;
         address seller;
-        Asset asset;
         uint amount;
         uint price;
     }
 
     uint private _listingId = 0;
-    mapping(uint => Listing) private _listings;
+    mapping(address => Listing) private _listings;
 
-    function listasset(address assetAddress, uint amount, uint price) public {
+    function createcontent(string memory _name, string memory _symbol) public returns (address) {
+        Asset asset = new Asset(_name, _symbol);
+        _assets.push(address(asset));
+        Listing memory listing = Listing(Listingstatus.Cancelled, msg.sender, 0, 0);
+        _listings[address(asset)]=listing;
+        asset.transferOwnership(msg.sender);
+        return address(asset);
+    }
+
+
+
+    
+
+    
+
+    function addassettolist(address assetAddress, uint amount, uint price) public {
         Asset asset = Asset(assetAddress);
         require(asset.transferFrom(msg.sender,address(this), amount), "Listing Failed");
-        Listing memory listing = Listing(Listingstatus.Active, msg.sender, asset, amount, price);
-        _listings[_listingId] = listing;
+        Listing memory listing = _listings[assetAddress];
+        listing.amount=amount;
+        listing.price=price;
+        listing.status=Listingstatus.Active;
+        _listings[assetAddress] = listing;
         _listingId++;
     }
 
-    function getListing(uint listingId) public view returns (Listing memory) {
-        return _listings[listingId];
+    function getListing(address assetAddress) public view returns (Listing memory) {
+        return _listings[assetAddress];
     }
 
-    function buyasset(uint listingId, uint numcoins) external payable {
-        Listing storage listing = _listings[listingId];
+    function buyasset(address assetAddress, uint numcoins) external payable {
+        Listing storage listing = _listings[assetAddress];
         require(listing.status == Listingstatus.Active, "Listing is not active");
         require(msg.sender != listing.seller, "Seller can't be the buyer");
         require(listing.amount >= numcoins, "Not enough tokens available");
         require(msg.value >= listing.price * numcoins, "Insufficient payment");
 
-        Asset asset = listing.asset;
+        Asset asset = Asset(assetAddress);
         asset.transfer(msg.sender, numcoins);
         payable(listing.seller).transfer(listing.price * numcoins);
         listing.amount -= numcoins;
@@ -64,15 +67,15 @@ contract Market {
         if (listing.amount == 0) {
             listing.status = Listingstatus.Sold;
         }
-        _listings[listingId] = listing;
+        _listings[assetAddress] = listing;
     }
 
-    function cancel(uint listingId) public {
-        Listing storage listing = _listings[listingId];
+    function cancel(address assetAddress) public {
+        Listing storage listing = _listings[assetAddress];
         require(msg.sender == listing.seller, "Only seller can cancel listing");
         require(listing.status == Listingstatus.Active, "Listing is not active");
         listing.status = Listingstatus.Cancelled;
-        Asset asset = listing.asset;
+        Asset asset = Asset(assetAddress);
         asset.transfer(msg.sender, listing.amount);
     }
 }
